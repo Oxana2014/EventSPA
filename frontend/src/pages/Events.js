f5c3398bdfbff3096a195a22c8ea98a9fd922134
Oksana Kormilina <keysb@mail.ru>
2024-02-13 15:59:04 +0400

    Return Response from loader()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,9 @@
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData()
+  const data = useLoaderData()
+  const events = data.events
+
   return (
     <>
        <EventsList events={events} />
@@ -15,12 +17,17 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+
+    //you can use in this function any browser features (cookies, localStorage ... )
+    // but you can not use hooks, because it is not inside of component
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
     //...
     } else {
-      const resData = await response.json();
-      return resData.events
+     // const resData = await response.json();
+     // return resData.events
+   //  const res = new Response('your data', {status: 201})
+     return response
     }
 }
