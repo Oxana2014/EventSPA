54e6fe0705c3b676159e3dde5940e41a16b46b32
Oksana Kormilina <keysb@mail.ru>
2024-02-14 15:05:17 +0400

    Delete event

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData, json } from "react-router-dom";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
@@ -18,4 +18,20 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({request, params}) {
+    const eventId = params.id
+   const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method
+  })
+   if (!response.ok) {
+    throw json(
+      { message: "Could not delete event" },
+      { status: 500 }
+    );
+  } else {
+    return redirect('/events');
+  }
+
 }
